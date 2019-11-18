97bda5588a26655b08cf9dc13a366c5c638fed67
ankunda <anky2k6@gmail.com>
2019-11-18 10:15:18 -0600

    spacing adjusted

--- trial.java
+++ trial.java
@@ -3,7 +3,7 @@ class trial
     public static void main(String [] args)
     {
 	System.out.println("Hello World");
-    System.out.println("a second line");
+	System.out.println("a second line");
     }
 }
 
